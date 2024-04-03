c569f23b03029d38044046db64f81cafe086585e
LamineOzilJr <lamzojr72@gmail.com>
2024-04-03 19:43:42 +0000

    daysearch

--- src/main/java/com/lamine/CalandarController.java
+++ src/main/java/com/lamine/CalandarController.java
@@ -1,5 +1,6 @@
 package com.lamine;
-import ORG.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
